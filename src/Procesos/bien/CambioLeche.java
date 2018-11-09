0c320e926a2aa98c44f4d0001659083d96e52bbc
Usuario <Usuario@DESKTOP-JF5JFGM>
2018-11-08 19:59:46 -0500

    Neythan Léon - usuario Modificacion del documento TallerSolid01.doc

---
+++ src/Procesos/bien/CambioLeche.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Procesos.Bien;
+import Leche.Bien.Leche;
+
+import Leche.LecheDeslactosada;
+import Leche.LecheDescremada;
+
+/**
+ *
+ * @author Pedro Mendoza
+ */
+public class CambioLeche {
+    
+    private Leche leche;
+
+    public CambioLeche(Leche leche) {
+        this.leche = leche;
+    }
+
+    public void cambiarLeche(){
+        this.leche.usar();
+        
+    }
+}
--- src/Procesos/bien/OperacionesAderezo.java
+++ src/Procesos/bien/OperacionesAderezo.java
@@ -3,24 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Procesos.bien;
- import Procesos.*;
+package Procesos.Bien;
+
+import Procesos.*;
 import Otros.Aderezo;
 import Postres.Pastel;
 import Postres.Helado;
 import Postres.bien.Postre;
- /**
+/**
  *
  * @author Pedro Mendoza
  */
 public class OperacionesAderezo {
     
-    public static void anadirAderezo(Postre postre,Aderezo aderezo){
+    public static void anadirAderezo(Postre postre, Aderezo aderezo){
         postre.getAderezos().add(aderezo);
     }
-    
-    public static void quitarAderezo(Postre postre,Aderezo aderezo){
+    public static void quitarAderezo(Postre postre, Aderezo aderezo){
         postre.getAderezos().remove(aderezo);
     }
-    
 }
